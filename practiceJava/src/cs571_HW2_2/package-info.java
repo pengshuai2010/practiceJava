19e1dcd00d25c97def060a9b799fcd20fb9a38a2
shuaipeng <shuaipeng@ENG401070>
2015-10-08 10:33:22 -0600

    add package info to describe design and refactoring

--- practiceJava/src/cs571_HW2_2/package-info.java
+++ practiceJava/src/cs571_HW2_2/package-info.java
@@ -1,5 +1,15 @@
 /**
+ * To support discounts for 3 different kind of customers, there should be 3 
+ * different data members representing for them in Sale class. So I renamed 
+ * DISCOUNT_RATE to DISCOUNT_RATE_NORMAL, and added DISCOUNT_RATE_SINIOR and 
+ * DISCOUNT_RATE_PREFERED.
  * 
+ * And I used Enum type to represent 3 kind of customers, namely SENIOR, PREFFERED
+ * and NORMAL. 
+ * 
+ * I refactored getDiscountRate() method to take an argument of Enum type Customer.
+ *  getDiscountRate() uses switch case statement to decide which discount rate is 
+ * applicable.
  */
 /**
  * @author speng
