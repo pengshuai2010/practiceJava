de1caad0202cdfaf2906cf10bac17b1b764c685c
shuaipeng <pengshuai2010@gmail.com>
2016-01-29 11:07:22 -0700

    solved a lintCode problem

---
+++ practiceJava/src/lintCode/Q2.java
@@ -0,0 +1,42 @@
+package lintCode;
+
+public class Q2 {
+	/*
+	 * Trailing Zeros Show result
+	 * 
+	 * You have exceeded the time limit Reset Write an algorithm which computes
+	 * the number of trailing zeros in n factorial.
+	 * 
+	 * Example 11! = 39916800, so the out should be 2
+	 */
+	/*
+	 * a O(1) solution. The number of trailing zeros is equal to number of
+	 * min(number of 5's, number of 2's). Since there far more 2's than 5's, it
+	 * is then equal to number of 5's. note that threre are two 5's instead of
+	 * one in the number 75.
+	 */
+	/**
+	 * @param n
+	 *            As desciption return:
+	 * @return An integer, denote the number of trailing zeros in n
+	 */
+	public long trailingZeros(long n) {
+		long numFives = 0;
+		long fivePower = 5;
+		long newFives = 0;
+		do {
+			newFives = n / fivePower;
+			numFives += newFives;
+			fivePower *= 5;
+		} while (newFives != 0);
+		return numFives;
+	}
+
+	public static void main(String[] args) {
+		long n = 11;
+		// long n = 105;
+		long res = new Q2().trailingZeros(n);
+		System.out.println(res);
+	}
+
+}
