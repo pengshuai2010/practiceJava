bc8e719a843451d9c2d6cc18d2c1a9799e32a531
shuaipeng <pengshuai2010@gmail.com>
2016-01-29 10:34:32 -0700

    add another solution for lintcode Q114

--- practiceJava/src/lintCode/Q114.java
+++ practiceJava/src/lintCode/Q114.java
@@ -17,11 +17,17 @@ public class Q114 {
 	 * 100.
 	 */
 	/**
+	 * n choose k approach, calculate (l + s)*(l + s-1)*...*(l +
+	 * 1)/s/(s-1)/.../2/1 (s-1) multiplies and (s-1) divides in total, where s =
+	 * min(m-1, n-1), l = max(m-1, n-1) so time complexity is O(min(m, n)) note
+	 * that intermediate result can be very large, we need use long or
+	 * BigInteger
+	 * 
 	 * @param n,
 	 *            m: positive integer (1 <= n ,m <= 100)
 	 * @return an integer
 	 */
-	public int uniquePaths(int m, int n) {
+	public int uniquePaths1(int m, int n) {
 		int large = Math.max(m - 1, n - 1);
 		int small = Math.min(m - 1, n - 1);
 		BigInteger res = BigInteger.valueOf(1L);
@@ -32,6 +38,23 @@ public class Q114 {
 			res = res.divide(BigInteger.valueOf(tmp));
 		}
 		return res.intValue();
+	}
+
+	/*
+	 * dynamic programming approach we need to fill up a m by n table, so time
+	 * complexity is O(m*n) note that unlike the n choose k approach, only
+	 * addition is involved so the constant part is much smaller
+	 */
+	public int uniquePaths(int m, int n) {
+		int[][] table = new int[m][n];
+		for (int i = 0; i < m; i++)
+			table[i][0] = 1;
+		for (int j = 0; j < n; j++)
+			table[0][j] = 1;
+		for (int i = 1; i < m; i++)
+			for (int j = 1; j < n; j++)
+				table[i][j] = table[i - 1][j] + table[i][j - 1];
+		return table[m - 1][n - 1];
 	}
 
 	public static void main(String[] args) {
