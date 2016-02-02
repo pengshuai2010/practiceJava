821abc2dc60f628d76a757ffce96861c9f4ecd67
shuaipeng <pengshuai2010@gmail.com>
2016-02-02 15:35:41 -0700

    solved Q182

--- practiceJava/src/lintCode/Q182.java
+++ practiceJava/src/lintCode/Q182.java
@@ -4,44 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Q182 {
-    /**
-     *@param A: A positive integer which has N digits, A is a string.
-     *@param k: Remove k digits.
-     *@return: A string
-     */
-    public String DeleteDigits(String A, int k) {
-	if (A == null || A.length() == 0 || A.length() <= k)
-	    return "";
-	List<Character> remain = new ArrayList<Character>();
-	for (int i = 0; i < A.length(); i++)
-	    remain.add(A.charAt(i));
-	StringBuilder builder = new StringBuilder();
-	for (int i = 0; i < A.length() - k; i++) {
-	    int leastIndex = 0;
-	    for (int j = 0; j <= k && j < remain.size(); j++) {
-		if (remain.get(j) < remain.get(leastIndex)) {
-		    leastIndex = j;
+	/**
+	 * @param A:
+	 *            A positive integer which has N digits, A is a string.
+	 * @param k:
+	 *            Remove k digits.
+	 * @return: A string
+	 */
+	public String DeleteDigits(String A, int k) {
+		if (A == null || A.length() == 0 || A.length() <= k)
+			return "";
+		StringBuilder builder = new StringBuilder(A);
+		for (int i = 0; i < k; i++) {
+			for (int j = 0; j < builder.length(); j++) {
+				if (j == builder.length() - 1 || builder.charAt(j) > builder.charAt(j + 1)) {
+					builder.deleteCharAt(j);
+					break;
+				}
+			}
 		}
-	    }
-	    builder.append(remain.remove(leastIndex));
-	    for (int j = 0; j < leastIndex; j++)
-		remain.remove(j);
+		while (builder.charAt(0) == '0')
+			builder.deleteCharAt(0);
+		return builder.toString();
 	}
-	return builder.toString();
-    }
-    public static void main(String[] args) {
-//	String A = "178542";
-//	int k = 4;
-//	String A = "";
-//	int k = 4;
-//	String A = "1234";
-//	int k = 4;
-//	String A = "1234";
-//	int k = 0;
-	String A = "254193";
-	int k = 1;
-	String res = new Q182().DeleteDigits(A, k);
-	System.out.println(res);
-    }
+
+	public static void main(String[] args) {
+		 String A = "178542";
+		 int k = 4;
+//		 String A = "";
+//		 int k = 4;
+//		 String A = "1234";
+//		 int k = 4;
+//		 String A = "1234";
+//		 int k = 0;
+//		String A = "254193";
+//		int k = 1;
+//		String A = "123456789";
+//		int k = 1;
+		String res = new Q182().DeleteDigits(A, k);
+		System.out.println(res);
+	}
 
 }
