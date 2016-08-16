e6718001f724bafceced3697ade3f35f62060b47
shuaipeng <pengshuai2010@gmail.com>
2016-08-16 17:17:49 -0600

    solve Q69

---
+++ practiceJava/src/lintCode/Q69.java
@@ -0,0 +1,47 @@
+package lintCode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by shuaipeng on 8/16/16.
+ */
+public class Q69 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(new Q69().levelOrder(root).toString());
+    }
+
+    /**
+     * @param root: The root of binary tree.
+     * @return: Level order a list of lists of integer
+     */
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> levels = new ArrayList<>();
+        // always remember to check for null!!!
+        if (root == null)
+            return levels;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ArrayList<Integer> level = new ArrayList<>();
+            // use this trick to traverse by level
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                level.add(current.val);
+                if (current.left != null)
+                    queue.offer(current.left);
+                if (current.right != null)
+                    queue.offer(current.right);
+            }
+            levels.add(level);
+        }
+        return levels;
+    }
+}
