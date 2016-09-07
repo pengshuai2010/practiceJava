ae7b2662b36b9e4af13677bbece2d82f8b9521ba
shuai peng <pengshuai2010@gmail.com>
2016-09-06 22:23:04 -0600

    low performance

--- practiceJava/src/leetCode/Q8stringToInteger.java
+++ practiceJava/src/leetCode/Q8stringToInteger.java
@@ -1,8 +1,6 @@
 package leetCode;
 
 import java.math.BigInteger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Created by shuaipeng on 9/6/16.
@@ -17,6 +15,10 @@ public class Q8stringToInteger {
         System.out.println(new Q8stringToInteger().myAtoi(" -12a   "));
         System.out.println(new Q8stringToInteger().myAtoi(" +12a   "));
         System.out.println(new Q8stringToInteger().myAtoi(" 12a   "));
+        System.out.println(new Q8stringToInteger().myAtoi("  -0012a42"));
+        System.out.println(new Q8stringToInteger().myAtoi(Integer.toString(Integer.MAX_VALUE)));
+        System.out.println(new Q8stringToInteger().myAtoi(Integer.toString(-Integer.MAX_VALUE)));
+        System.out.println(new Q8stringToInteger().myAtoi(Integer.toString(Integer.MIN_VALUE)));
         System.out.println(new Q8stringToInteger().myAtoi("9223372036854775809"));
         System.out.println(new Q8stringToInteger().myAtoi("-9223372036854775809"));
     }
@@ -49,16 +51,59 @@ public class Q8stringToInteger {
     public int myAtoi(String str) {
         if (str == null || str.length() == 0)
             return 0;
-        // the benefits of using regular expressions is that, when requirements change, we don't need to change the
-        // the logic, only need to change the regex pattern
-        Pattern p = Pattern.compile("\\s*([+-]?)(\\d+)[^\\d]*");
-        Matcher m = p.matcher(str);
-        if (!m.matches()) {
-            System.out.println(str + ": invalid");
-            return 0;
-        }
-        System.out.println(str + ":  " + m.group(1) + ", " + m.group(2));
-        return 0;
 
+//        // the benefits of using regular expressions is that, when requirements change, we don't need to change the
+//        // the logic, only need to change the regex pattern
+//        Pattern p = Pattern.compile("\\s*([+-]?)(\\d+)[^\\d]*.*");
+//        Matcher m = p.matcher(str);
+//        if (!m.matches())
+//            return 0;
+//        boolean isNegative = false;
+//        String signPart = m.group(1);
+//        String numPart = m.group(2);
+
+        String sb = str.trim();
+        if (sb.length() == 0)
+            return 0;
+        char leadingCh = sb.charAt(0);
+        if (leadingCh != '-' && leadingCh != '+' && !Character.isDigit(leadingCh))
+            return 0;
+        int endIndex = 1;
+        while (endIndex < sb.length() && Character.isDigit(sb.charAt(endIndex)))
+            endIndex++;
+        if (!Character.isDigit(leadingCh) && endIndex == 1)
+            return 0;
+        String numPart;
+        String signPart = "";
+        if (Character.isDigit(leadingCh)) {
+            numPart = sb.substring(0, endIndex);
+        } else {
+            numPart = sb.substring(1, endIndex);
+            signPart = sb.substring(0, 1);
+        }
+        boolean isNegative = false;
+
+        if (signPart != null && signPart.length() != 0 && signPart.charAt(0) == '-')
+            isNegative = true;
+        long number = 0;
+        for (int i = 0; i < numPart.length(); i++) {
+            int val = numPart.charAt(i) - '0';
+            // another way to detect overflow is
+            // "if (number > (Integer.MAX_VALUE - val)/10)" before we update number
+            number = number * 10 + val;
+            // note that MAX_VALUE is 2147483647, and MIN_VALUE is -2147483648
+            // the absolute value of MAX_VALUE is 1 less than MIN_VALUE
+            // so if we use "number >= Integer.MAX_VALUE" here, it will fail the input "-2147483647"
+            if (number > Integer.MAX_VALUE)
+                if (isNegative)
+                    return Integer.MIN_VALUE;
+                else
+                    return Integer.MAX_VALUE;
+        }
+        if (isNegative)
+            number = -number;
+        return (int) number;
     }
+
+
 }
