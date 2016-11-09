1d02c0b987162ac6956443edb8555c3a3d148f1b
shuai peng <pengshuai2010@gmail.com>
2016-11-08 22:13:16 -0700

    Q20

--- practiceJava/src/leetCode/Q20_ValidParentheses.java
+++ practiceJava/src/leetCode/Q20_ValidParentheses.java
@@ -6,73 +6,6 @@ import java.util.Stack;
  * Created by speng on 11/8/16.
  */
 public class Q20_ValidParentheses {
-    public boolean isValid(String s) {
-        if (s == null) {
-            // deal with invalid input
-        }
-        if (s.length() % 2 != 0)
-            return false;
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            switch (ch) {
-                case '{':
-                case '(':
-                case '[':
-                    stack.push(ch);
-                    break;
-                case '}':
-                    if (stack.isEmpty() || stack.pop() != '{') // don't forget check if stack empty before pop!
-                        return false;
-                    break;
-                case ')':
-                    if (stack.isEmpty() || stack.pop() != '(')
-                        return false;
-                    break;
-                case ']':
-                    if (stack.isEmpty() || stack.pop() != '[')
-                        return false;
-                    break;
-                default:
-                    //handle error here
-            }
-        }
-        return stack.isEmpty();
-    }
-
-    public boolean isValid(String s) {
-        if (s == null) {
-            // deal with invalid input
-        }
-        if (s.length() % 2 != 0)
-            return false;
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            switch (ch) {
-                case '{':
-                case '(':
-                case '[':
-                    stack.push(ch);
-                    break;
-                case '}':
-                    if (stack.isEmpty() || stack.pop() != '{') // don't forget check if stack empty before pop!
-                        return false;
-                    break;
-                case ')':
-                    if (stack.isEmpty() || stack.pop() != '(')
-                        return false;
-                    break;
-                case ']':
-                    if (stack.isEmpty() || stack.pop() != '[')
-                        return false;
-                    break;
-                default:
-                    //handle error here
-            }
-        }
-        return stack.isEmpty();
-    }
 
     public static void main(String[] args) {
         Q20_ValidParentheses solution = new Q20_ValidParentheses();
@@ -81,5 +14,39 @@ public class Q20_ValidParentheses {
             System.out.println(input);
             System.out.println(solution.isValid(input));
         }
+    }
+
+    public boolean isValid(String s) {
+        if (s == null) {
+            // deal with invalid input
+        }
+        if (s.length() % 2 != 0)
+            return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            switch (ch) {
+                case '{':
+                case '(':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{') // don't forget check if stack empty before pop!
+                        return false;
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(')
+                        return false;
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[')
+                        return false;
+                    break;
+                default:
+                    //handle error here
+            }
+        }
+        return stack.isEmpty();
     }
 }
