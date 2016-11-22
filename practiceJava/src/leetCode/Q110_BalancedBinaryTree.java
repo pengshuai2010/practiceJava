613ca8610b0a708732556d1e5bf928256a8b0fc2
shuai peng <pengshuai2010@gmail.com>
2016-11-22 15:07:48 -0700

    Q63

---
+++ practiceJava/src/leetCode/Q110_BalancedBinaryTree.java
@@ -0,0 +1,30 @@
+package leetCode;
+
+import lintCode.TreeNode;
+
+/**
+ * Created by speng on 11/22/16.
+ */
+public class Q110_BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        return isBalancedRecursive(root) != -1;
+    }
+
+    /**
+     * similar to get binary tree height but use -1 to indicate imbalancement
+     * time complexity O(n), where n is the number of nodes in the tree
+     */
+    private int isBalancedRecursive(TreeNode root) {
+        if (root == null)
+            return 0;
+        int leftHeight = isBalancedRecursive(root.left);
+        if (leftHeight == -1)
+            return -1;
+        int rightHeight = isBalancedRecursive(root.right);
+        if (rightHeight == -1)
+            return -1;
+        if (Math.abs(leftHeight - rightHeight) > 1)
+            return -1;
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
