80adb2175ca6da89eedc7c3b5b864b3392f0117a
shuai peng <pengshuai2010@gmail.com>
2016-11-22 13:21:38 -0700

    lintcode Q378

---
+++ practiceJava/src/lintCode/DoublyListNode.java
@@ -0,0 +1,14 @@
+package lintCode;
+
+/**
+ * Created by speng on 11/22/16.
+ */
+public class DoublyListNode {
+    int val;
+    DoublyListNode next, prev;
+
+    DoublyListNode(int val) {
+        this.val = val;
+        this.next = this.prev = null;
+    }
+}
---
+++ practiceJava/src/lintCode/Q378ConvertBinarySearchTreetoDoublyLinkedList.java
@@ -0,0 +1,29 @@
+package lintCode;
+
+/**
+ * Created by speng on 11/22/16.
+ */
+public class Q378ConvertBinarySearchTreetoDoublyLinkedList {
+    /**
+     * @param root: The root of tree
+     * @return: the head of doubly list node
+     */
+    public DoublyListNode bstToDoublyList(TreeNode root) {
+        DoublyListNode dummy = new DoublyListNode(0);
+        DoublyListNode tail = dummy;
+        inorder(root, tail);
+        return dummy.next;
+    }
+
+    private DoublyListNode inorder(TreeNode root, DoublyListNode tail) {
+        if (root == null)
+            return tail;
+        tail = inorder(root.left, tail);
+        DoublyListNode node = new DoublyListNode(root.val);
+        tail.next = node;
+        node.prev = tail;
+        tail = node;
+        tail = inorder(root.right, tail);
+        return tail;
+    }
+}
