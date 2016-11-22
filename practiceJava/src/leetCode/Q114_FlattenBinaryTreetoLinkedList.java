87671477ea344f2fcf054aa4b3863499c0a9847a
shuai peng <pengshuai2010@gmail.com>
2016-11-22 12:47:43 -0700

    Q114

---
+++ practiceJava/src/leetCode/Q114_FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,37 @@
+package leetCode;
+
+import lintCode.TreeNode;
+
+/**
+ * Created by speng on 11/22/16.
+ */
+public class Q114_FlattenBinaryTreetoLinkedList {
+    public void flatten(TreeNode root) {
+        flattenRecusive(root);
+        return;
+    }
+
+    /**
+     * return the last node of the flatterned tree
+     */
+    private TreeNode flattenRecusive(TreeNode root) {
+        if (root == null || root.left == null && root.right == null)
+            return root;
+        TreeNode lSubtree = root.left;
+        TreeNode rSubtree = root.right;
+        TreeNode lEnd = flattenRecusive(lSubtree);
+        TreeNode rEnd = flattenRecusive(rSubtree);
+        if (root.right == null) {
+            root.right = lSubtree;
+            root.left = null;
+            return lEnd;
+        }
+        if (root.left == null) {
+            return rEnd;
+        }
+        root.right = lSubtree;
+        root.left = null;
+        lEnd.right = rSubtree;
+        return rEnd;
+    }
+}
