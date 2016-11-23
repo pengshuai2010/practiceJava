acf216d9a0971120208b3875b10d9f282dc4f768
shuai peng <pengshuai2010@gmail.com>
2016-11-23 13:27:20 -0700

    corrrected an error

--- practiceJava/src/lintCode/Q96.java
+++ practiceJava/src/lintCode/Q96.java
@@ -3,11 +3,16 @@ package lintCode;
 import basicAlgorithms.ListNode;
 
 public class Q96 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
     /**
      * @param head: The first node of linked list.
      * @param x: an integer
-     * @return: a ListNode 
-     */
+	 * @return: a ListNode
+	 */
     public ListNode partition1(ListNode head, int x) {
     	ListNode l1 = null;
     	ListNode l2 = null;
@@ -57,34 +62,5 @@ public class Q96 {
     	t1.next = l2;
     	return l1;
     }
-    
-    public ListNode partition(ListNode head, int x) {
-    	ListNode l1 = new ListNode(0);
-    	ListNode t1 = l1;
-    	ListNode l2 = new ListNode(0);
-    	ListNode t2 = l2;
-    	ListNode p = head;
-    	while (p != null) {
-    		ListNode tmp = p;
-    		p = p.next;
-    		tmp.next = null;
-    		if (tmp.val < x) {
-    			t1.next = tmp;
-    			t1 = t1.next;
-    		} else {
-    			t2.next = tmp;
-    			t2 = t2.next;
-    		}
-    	}
-    	l1 = l1.next;
-    	l2 = l2.next;
-    	if (l1 == null)
-    		return l2;
-    	t1.next = l2;
-    	return l1;
-    }	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
