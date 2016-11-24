a1f4a5b003f0776c8c60aae6b4ac4ab594cd77f5
shuai peng <pengshuai2010@gmail.com>
2016-11-23 22:33:41 -0700

    use queue in traverse by layer

--- practiceJava/src/leetCode/Q102_BinaryTreeLevelOrderTraversal.java
+++ practiceJava/src/leetCode/Q102_BinaryTreeLevelOrderTraversal.java
@@ -3,27 +3,34 @@ package leetCode;
 import basicAlgorithms.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by speng on 11/23/16.
  */
 public class Q102_BinaryTreeLevelOrderTraversal {
+
+    /**
+     * traverse by layer, a variant of BFS
+     */
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> layers = new ArrayList<>();
         if (root == null)
             return layers;
-        List<TreeNode> curr = new ArrayList<>();
-        curr.add(root);
+        Queue<TreeNode> curr = new LinkedList<>();
+        curr.offer(root);
         while (curr.size() != 0) {
-            List<TreeNode> next = new ArrayList<>();
+            Queue<TreeNode> next = new LinkedList<>();
             List<Integer> list = new ArrayList<>();
-            for (TreeNode node : curr) {
+            while (!curr.isEmpty()) {
+                TreeNode node = curr.poll();
                 list.add(node.val);
                 if (node.left != null)
-                    next.add(node.left);
+                    next.offer(node.left);
                 if (node.right != null)
-                    next.add(node.right);
+                    next.offer(node.right);
             }
             layers.add(list);
             curr = next;
