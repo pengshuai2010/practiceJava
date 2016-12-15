ca2b49b2bd13ae14f86001b070ce4c185c323834
shuai peng <pengshuai2010@gmail.com>
2016-12-15 10:24:13 -0700

    Q64  add O(1) DP solution

--- practiceJava/src/leetCode/Q64_MinimumPathSum.java
+++ practiceJava/src/leetCode/Q64_MinimumPathSum.java
@@ -5,7 +5,7 @@ package leetCode;
  */
 public class Q64_MinimumPathSum {
     /**
-     * takes O(mn) time, O(n) space
+     * takes O(mn) time, O(n) space.
      */
     public int minPathSum1(int[][] grid) {
         if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
@@ -14,13 +14,13 @@ public class Q64_MinimumPathSum {
         int m = grid.length;
         int n = grid[0].length;
         int[] prev = new int[n];
-        prev[0] = grid[0][0];
+        prev[0] = grid[0][0];//don't forget initial condition.
         for (int j = 1; j < n; j++) {
             prev[j] = prev[j - 1] + grid[0][j];
         }
         for (int i = 1; i < m; i++) {
             int[] curr = new int[n];
-            curr[0] = grid[i][0] + prev[0];
+            curr[0] = grid[i][0] + prev[0];//special attention to first column of each row
             for (int j = 1; j < n; j++) {
                 curr[j] = grid[i][j] + Math.min(prev[j], curr[j - 1]);
             }
@@ -31,11 +31,8 @@ public class Q64_MinimumPathSum {
 
     /**
      * takes O(mn) time, O(mn) space
-     *
-     * @param grid
-     * @return
      */
-    public int minPathSum(int[][] grid) {
+    public int minPathSum2(int[][] grid) {
         if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
             return -1;
         }
@@ -55,5 +52,28 @@ public class Q64_MinimumPathSum {
             }
         }
         return dp[m - 1][n - 1];
+    }
+
+    /**
+     * write the DP result in-place, takes O(1) space. And code is much cleaner than O(n) space solution.
+     */
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
+            return -1;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+        for (int j = 1; j < n; j++) {
+            grid[0][j] += grid[0][j - 1];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[m - 1][n - 1];
     }
 }
