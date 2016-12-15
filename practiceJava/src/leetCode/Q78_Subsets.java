a7979d1ee50f3bfa4e8fc0cfdfbc1b6fa27ebaf2
shuai peng <pengshuai2010@gmail.com>
2016-12-14 19:31:42 -0700

    Q78

--- practiceJava/src/leetCode/Q78_Subsets.java
+++ practiceJava/src/leetCode/Q78_Subsets.java
@@ -7,7 +7,7 @@ import java.util.List;
  * Created by shuaipeng on 11/30/16.
  */
 public class Q78_Subsets {
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsets1(int[] nums) {
         List<List<Integer>> solutions = new ArrayList<>();
         if (nums == null || nums.length == 0) {
             solutions.add(new ArrayList<>());//add the empty set
@@ -28,5 +28,31 @@ public class Q78_Subsets {
             dfs(nums, i + 1, partial, solutions);
             partial.remove(partial.size() - 1);
         }
+    }
+
+    /**
+     * use integer in range [0, 1 << n) to represent 2^n combinations. This is easier than DFS.
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> solutions = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            solutions.add(new ArrayList<>());//add the empty set
+            return solutions;
+        }
+        for (int i = 0; i < (1 << nums.length); i++) {
+            solutions.add(getSubset(nums, i));
+        }
+        return solutions;
+    }
+
+    private List<Integer> getSubset(int[] nums, int x) {
+        int mask = 1 << (nums.length - 1);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++, mask >>= 1) {
+            if ((x & mask) > 0) {
+                list.add(nums[i]);
+            }
+        }
+        return list;
     }
 }
