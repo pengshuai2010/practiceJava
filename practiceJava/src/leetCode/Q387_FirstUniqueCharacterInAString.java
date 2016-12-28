a65e0ac538152e130e66afb95849eca22614d512
shuaipeng <pengshuai2010@gmail.com>
2016-12-28 13:03:21 -0700

    Q378 add another solution

--- practiceJava/src/leetCode/Q387_FirstUniqueCharacterInAString.java
+++ practiceJava/src/leetCode/Q387_FirstUniqueCharacterInAString.java
@@ -4,7 +4,10 @@ package leetCode;
  * Created by shuaipeng on 12/28/16.
  */
 public class Q387_FirstUniqueCharacterInAString {
-    public int firstUniqChar(String s) {
+    /**
+     * When dealing with this kind of problems, always clarify the character set.
+     */
+    public int firstUniqChar1(String s) {
         if (s == null || s.length() == 0) {
             return -1;
         }
@@ -18,5 +21,32 @@ public class Q387_FirstUniqueCharacterInAString {
             }
         }
         return -1;
+    }
+
+    /**
+     * when string is very long, we can save some time by saving index of first occurrence
+     */
+    public int firstUniqChar(String s) {
+        if (s == null || s.length() == 0) {
+            return -1;
+        }
+        int[] frequency = new int[26];
+        int[] pos = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            frequency[s.charAt(i) - 'a']++;
+            if (frequency[s.charAt(i) - 'a'] == 1) {
+                pos[s.charAt(i) - 'a'] = i;
+            }
+        }
+        int firstUnique = s.length();
+        for (int i = 0; i < 26; i++) {
+            if (frequency[i] == 1) {
+                firstUnique = Math.min(firstUnique, pos[i]);
+            }
+        }
+        if (firstUnique == s.length()) {
+            return -1;
+        }
+        return firstUnique;
     }
 }
