8d61f8578c0db480b1ad90ddd2030d95728a08ca
shuai peng <pengshuai2010@gmail.com>
2017-01-16 21:08:34 -0700

    Q14 more elegant code

---
+++ practiceJava/src/lintCode/Q14_FirstPositionOfTarget.java
@@ -0,0 +1,34 @@
+package lintCode;
+
+public class Q14_FirstPositionOfTarget {
+    /**
+     * @param nums:   The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+    public int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                high = mid;
+            }
+        }
+        //we are looking for first position, so check nums[low] first
+        if (nums[low] == target) {
+            return low;
+        }
+        if (nums[high] == target) {
+            return high;
+        }
+        return -1;
+    }
+}
