b6272278477d2ec3c06a23dae7021e23f7475010
shuai peng <pengshuai2010@gmail.com>
2017-01-16 23:30:42 -0700

    Q183 refactor

---
+++ practiceJava/src/lintCode/Q183_WoodCut.java
@@ -0,0 +1,45 @@
+package lintCode;
+
+public class Q183_WoodCut {
+    /**
+	 * @param L:
+	 *            Given n pieces of wood with length L[i]
+	 * @param k:
+	 *            An integer return: The maximum length of the small pieces.
+	 */
+	public int woodCut(int[] L, int k) {
+		if (L == null || L.length == 0)
+			return 0;
+		long sum = 0;
+		for (int item : L)
+			sum += item;
+        int high = (int) (sum / k);
+        if (high == 0) {//to avoid divided by zero error, handle the case where sum < k
+            return 0;
+        }
+        int low = 1;
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            int pieces = calcNumPieces(L, mid);
+            if (pieces >= k) {
+                low = mid;
+            } else if (pieces < k) {
+                high = mid - 1;
+            }
+        }
+        if (calcNumPieces(L, high) >= k) {
+            return high;
+        }
+        if (calcNumPieces(L, low) >= k) {
+            return low;
+        }
+        return 0;
+    }
+
+    private int calcNumPieces(int[] L, int l) {
+        int numPieces = 0;
+		for (int item : L)
+			numPieces += item / l;
+		return numPieces;
+	}
+}
