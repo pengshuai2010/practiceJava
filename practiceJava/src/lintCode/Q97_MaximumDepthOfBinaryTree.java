c0f37b16e1e0d32f1f3ded42eb3f197db952d5af
shuai peng <pengshuai2010@gmail.com>
2017-01-22 20:21:59 -0700

    Q97 add pre-order traversal solution

--- practiceJava/src/lintCode/Q97_MaximumDepthOfBinaryTree.java
+++ practiceJava/src/lintCode/Q97_MaximumDepthOfBinaryTree.java
@@ -13,10 +13,32 @@ public class Q97_MaximumDepthOfBinaryTree {
      * @param root: The root of binary tree.
      * @return: An integer.
      */
-    public int maxDepth(TreeNode root) {
+    public int maxDepth1(TreeNode root) {
         if (root == null) {
             return 0;
         }
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    private int maxDepth;
+
+    public int maxDepth(TreeNode root) {
+        maxDepth = 0;
+        preorder(root, 0);
+        return maxDepth;
+    }
+
+    /**
+     * pre-order traversal. Use instance variable to keep track of max depth we had ever been, use input argument to
+     * keep track of which layer we are on now.
+     */
+    private void preorder(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        depth++;
+        maxDepth = Math.max(maxDepth, depth);
+        preorder(root.left, depth);
+        preorder(root.right, depth);
     }
 }
