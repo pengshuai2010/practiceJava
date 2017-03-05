06749de47f75a66961895b7966edf23c0b170308
shuai peng <pengshuai2010@gmail.com>
2017-03-05 16:08:29 -0700

    Q608

---
+++ practiceJava/src/lintCode/Q608_TwoSum_InputArrayIsSorted.java
@@ -0,0 +1,30 @@
+package lintCode;
+
+/**
+ * Created by speng on 3/5/17.
+ */
+public class Q608_TwoSum_InputArrayIsSorted {
+    /*
+     * @param nums an array of Integer
+     * @param target = nums[index1] + nums[index2]
+     * @return [index1 + 1, index2 + 1] (index1 < index2)
+     */
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return null;
+        }
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum < target) {
+                i++;
+            } else if (sum > target) {
+                j--;
+            } else {
+                return new int[]{i + 1, j + 1};
+            }
+        }
+        return null;
+    }
+}
