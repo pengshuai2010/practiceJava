17e89c6481572bed085346c217054ccfd4d1ab4a
shuai peng <pengshuai2010@gmail.com>
2017-04-27 21:33:46 -0600

    Q585

---
+++ practiceJava/src/lintCode/Q585_MaximumNumberInMountainSequence.java
@@ -0,0 +1,27 @@
+package lintCode;
+
+/**
+ * Created by speng on 4/27/17.
+ */
+public class Q585_MaximumNumberInMountainSequence {
+    /**
+     * @param nums a mountain sequence which increase firstly and then decrease
+     * @return then mountain top
+     */
+    public int mountainSequence(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid - 1] > nums[mid]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        return Math.max(nums[start], nums[end]);
+    }
+}
